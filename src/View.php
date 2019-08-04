0ea4d912353cd18a31bd5a28ea363207f7305341
June McIntyre <junipermcintyre@gmail.com>
2019-08-04 02:17:36 -0400

    For server testing

--- src/View.php
+++ src/View.php
@@ -11,11 +11,17 @@ class View {
 		$this->user_properties = $user_properties;
 		$this->template = $this->constructTemplate();
 
+		var_dump($SERVER['SCRIPT_NAME']);
 		$base_url_parts = explode('/', $_SERVER['SCRIPT_NAME']);
+		var_dump($base_url_parts);
 		array_pop($base_url_parts);
+		var_dump($base_url_parts);
 		$base_url = implode('/', $base_url_parts);
+		var_dump($base_url);
 		if ($base_url !== '')
 			$base_url = '/'.$base_url;
+		var_dump($base_url);
+		die();
 		$this->base_url = $base_url;
 	}
 
