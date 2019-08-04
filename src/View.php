5ac1d98498f5f421bd4e6f459a4db3617be6a93f
June McIntyre <junipermcintyre@gmail.com>
2019-08-04 02:22:06 -0400

    more fiddling

--- src/View.php
+++ src/View.php
@@ -11,18 +11,11 @@ class View {
 		$this->user_properties = $user_properties;
 		$this->template = $this->constructTemplate();
 
-		var_dump($_SERVER);
-		var_dump($_SERVER['SCRIPT_NAME']);
 		$base_url_parts = explode('/', $_SERVER['SCRIPT_NAME']);
-		var_dump($base_url_parts);
 		array_pop($base_url_parts);
-		var_dump($base_url_parts);
 		$base_url = implode('/', $base_url_parts);
-		var_dump($base_url);
-		if ($base_url !== '')
-			$base_url = '/'.$base_url;
-		var_dump($base_url);
-		die();
+		if ($base_url === '')
+			$base_url = '/';
 		$this->base_url = $base_url;
 	}
 
